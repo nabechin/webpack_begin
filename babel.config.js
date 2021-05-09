9edcb4b33e698eb1fef9ad0e27a5795ab54b4d41
nabechin <nabenabe0258@gmail.com>
2021-05-09 23:28:01 +0900

    particular polifil import

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,9 @@
 module.exports = {
   presets: [
     '@babel/preset-env',
+    // polifylが必要な箇所に全てimportすると、全てのpolifilをimportすることになり、
+    // build時にファイルサイズが大きくなる。
+    // asyncやpromiseなど、使っている箇所だけpolifilを取り込むために記述
     { useBuildIns: 'usage', corejs: 3, debug: true },
   ],
 };
--- src/js/app.js
+++ src/js/app.js
@@ -5,6 +5,9 @@ $('body')
   .append(result)
   .append(`<p>${greet('app')}</p>`);
 
+// これらはES2015の構文なので、ES2015に対応していないブラウザは
+// 動作しない。なので、polifilを使い、擬似的に変換する。
+//
 const promise = new Promise((resolve) => {
   setTimeout(() => resolve('hello!'), 3000);
 });
