893c947acab105a2cc552a5546ed800770079e89
nabechin <nabenabe0258@gmail.com>
2021-05-11 22:36:55 +0900

    vender prefix add

--- webpack.common.js
+++ webpack.common.js
@@ -23,7 +23,12 @@ module.exports = {
       },
       {
         test: /\.scss$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'postcss-loader',
+          'sass-loader',
+        ],
       },
       {
         test: /\.js$/,
